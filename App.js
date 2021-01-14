d64763822fd0c457edc9aa86a31cd6846a76cf39
Alexandra <sandra.larrosa88@gmail.com>
2021-01-14 12:44:31 +0100

    check logged user

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import firebase from './src/utils/firebase';
+import 'firebase/auth';
 
 export default function App() {
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((response) => {
+      setUser(response);
+    });
+  }, []);
+
+  if (user === undefined) return null;
+
   return (
     <SafeAreaView>
-      <Text>Hola Mundo</Text>
+      {user ? <Text>Estás logueado</Text> : <Text>No estás logueado</Text>}
     </SafeAreaView>
   );
 }
