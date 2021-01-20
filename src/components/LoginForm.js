689420f4860ff67386da05686e20155789ccb107
Alexandra <sandra.larrosa88@gmail.com>
2021-01-20 18:21:13 +0100

    login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import {validateEmail} from '../utils/validation';
+import firebase from '../utils/firebase';
 
 export default function LoginForm(props) {
   const {changeForm} = props;
 
+  const login = () => {
+    console.log('Iniciando sesión');
+  };
   return (
-    <View>
-      <Text>LoginForm</Text>
-      <TouchableOpacity onPress={changeForm}>
-        <Text style={styles.btnText}>Regístrate</Text>
+    <>
+      <TextInput
+        style={styles.input}
+        placeholder="Correo electrónico"
+        placeholderTextColor="#969696"
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        placeholderTextColor="#969696"
+        secureTextEntry={true}
+      />
+      <TouchableOpacity onPress={login}>
+        <Text style={styles.btnText}>Iniciar Sesión</Text>
       </TouchableOpacity>
-    </View>
+      <View style={styles.register}>
+        <TouchableOpacity onPress={changeForm}>
+          <Text style={styles.btnText}>Regístrate</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -18,5 +45,22 @@ const styles = StyleSheet.create({
   btnText: {
     color: 'white',
     fontSize: 18,
+  },
+  input: {
+    height: 50,
+    color: 'white',
+    width: '80%',
+    marginBottom: 25,
+    backgroundColor: '#1E3040',
+    paddingHorizontal: 20,
+    borderRadius: 50,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#1E3040',
+  },
+  register: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 30,
   },
 });
