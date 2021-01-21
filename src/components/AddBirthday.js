968563930e04c13c49ee639cc00e720a9fd57b7d
Alexandra <sandra.larrosa88@gmail.com>
2021-01-21 12:32:58 +0100

    added form for creating birthay dates

--- src/components/AddBirthday.js
+++ src/components/AddBirthday.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function AddBirthday() {
   return (
-    <View>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-      <Text>AddBirthday</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre"
+          placeholderTextColor="#969696"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Apellidos"
+          placeholderTextColor="#969696"
+        />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 50,
+    color: 'white',
+    width: '80%',
+    marginBottom: 25,
+    backgroundColor: '#1e3040',
+    paddingHorizontal: 20,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#1e3040',
+    borderRadius: 50,
+  },
+});
