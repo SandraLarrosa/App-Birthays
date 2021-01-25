1d58efa719ed62d08f76340bf6a9e74368b6082c
Alexandra <sandra.larrosa88@gmail.com>
2021-01-25 17:50:23 +0100

    show the days for birthday and styles days

--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -5,6 +5,20 @@ export default function Birthday(props) {
   const {birthday} = props;
   const pasat = birthday.days > 0 ? true : false;
 
+  const infoDay = () => {
+    if (birthday.days === 0) {
+      return <Text style={styles.days}>Hoy es su cumpleaños</Text>;
+    } else {
+      const days = -birthday.days;
+      return (
+        <View style={styles.textCurrent}>
+          <Text>{days}</Text>
+          <Text>{days === 1 ? 'Día' : 'Días'}</Text>
+        </View>
+      );
+    }
+  };
+
   return (
     <TouchableOpacity
       style={[
@@ -18,6 +32,7 @@ export default function Birthday(props) {
       <Text>
         {birthday.name} {birthday.lastName}
       </Text>
+      {pasat ? <Text style={styles.days}>Pasado</Text> : infoDay()}
     </TouchableOpacity>
   );
 }
@@ -35,4 +50,14 @@ const styles = StyleSheet.create({
   current: {backgroundColor: '#1ae1f2'},
   pasat: {backgroundColor: '#820000'},
   actual: {backgroundColor: '#559204'},
+  days: {
+    color: 'white',
+  },
+  textCurrent: {
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
