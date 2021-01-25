8baab765223e2a0916630f71a74a80bc151f7adc
Alexandra <sandra.larrosa88@gmail.com>
2021-01-25 17:37:51 +0100

    styles birthdays

--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 export default function Birthday(props) {
   const {birthday} = props;
+  const pasat = birthday.days > 0 ? true : false;
+
   return (
-    <View>
+    <TouchableOpacity
+      style={[
+        styles.card,
+        pasat
+          ? styles.pasat
+          : birthday.days === 0
+          ? styles.actual
+          : styles.current,
+      ]}>
       <Text>
         {birthday.name} {birthday.lastName}
       </Text>
-    </View>
+    </TouchableOpacity>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 60,
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    margin: 10,
+    borderRadius: 15,
+  },
+  current: {backgroundColor: '#1ae1f2'},
+  pasat: {backgroundColor: '#820000'},
+  actual: {backgroundColor: '#559204'},
+});
