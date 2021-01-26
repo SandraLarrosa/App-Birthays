3516ff9f0a62c8f82ecab7b1fa525e93898292d9
Alexandra <sandra.larrosa88@gmail.com>
2021-01-26 13:53:40 +0100

    alert warning of today's birthdays

--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Alert} from 'react-native';
+import moment from 'moment';
 
 export default function Birthday(props) {
   const {birthday, deleteBirthday} = props;
   const pasat = birthday.days > 0 ? true : false;
 
+  const dateBirthFormat = moment(birthday.dateBirth).format('L');
+  console.log(dateBirthFormat);
+
   const infoDay = () => {
     if (birthday.days === 0) {
-      return <Text style={styles.days}>Hoy es su cumpleaños</Text>;
+      Alert.alert(`Hoy es el cumpleaños de ${birthday.name}`);
+      return <Text style={styles.days}>¡¡¡Hoy es su cumpleaños!!!</Text>;
     } else {
       const days = -birthday.days;
       return (
-        <View style={styles.textCurrent}>
-          <Text>{days}</Text>
-          <Text>{days === 1 ? 'Día' : 'Días'}</Text>
-        </View>
+        <>
+          <View style={styles.textCurrent}>
+            <Text style={styles.daysLeft}>
+              Faltan: {days} {days === 1 ? 'Día' : 'Días'}
+            </Text>
+            <Text style={styles.dayBirth}>{dateBirthFormat}</Text>
+          </View>
+        </>
       );
     }
   };
@@ -33,7 +42,7 @@ export default function Birthday(props) {
       <Text style={styles.name}>
         {birthday.name} {birthday.lastName}
       </Text>
-      {pasat ? <Text style={styles.days}>Ya ha pasado</Text> : infoDay()}
+      {pasat ? <Text style={styles.days}>{dateBirthFormat}</Text> : infoDay()}
     </TouchableOpacity>
   );
 }
@@ -56,10 +65,21 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 16,
   },
+  dayBirth: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  daysLeft: {
+    color: '#202040',
+    fontSize: 16,
+    padding: 1,
+  },
   textCurrent: {
-    backgroundColor: '#fff',
+    backgroundColor: '#f4a548',
     borderRadius: 20,
-    width: 50,
+    width: 150,
+    height: 50,
     alignItems: 'center',
     justifyContent: 'center',
   },
